a771f83e6a502ab8e03a293aa10351c60b938e4a
RobBloxham <rob.bloxham209@gmail.com>
2024-06-08 15:01:05 -0500

    back end complete

--- src/components/TopicsList.jsx
+++ src/components/TopicsList.jsx
@@ -2,6 +2,20 @@ import RemoveBtn from "./RemoveBtn";
 import Link from "next/link";
 import {HiPencilAlt} from "react-icons/hi";
 
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: 'no-store',
+    });
+    if(!res.ok) {
+      throw new Error('Network response was not ok');
+    } 
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export default function TopicsList() {
   return (
     <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
