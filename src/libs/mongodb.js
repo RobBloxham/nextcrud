f7e7f292ff8e83f157c7acf99c8f40ae7e842da8
RobBloxham <rob.bloxham209@gmail.com>
2024-06-08 14:35:09 -0500

    create functionalityp added

---
+++ src/libs/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectMongoDB =  async () => {
+    try {
+       await mongoose.connect(process.env.MONGODB_URI, 
+          console.log('connected to mongoDB')
+        );
+    } catch (error) {
+        console.log("MongoDB connection failed", error);
+    }
+};
+
+export default connectMongoDB;
